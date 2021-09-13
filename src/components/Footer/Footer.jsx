e108e1f3b5bea4dad205f4acd4bf4901c43e99b3
Kevin Milet <kevin.milet@gmail.com>
2021-09-13 16:41:20 +0200

    kevin - ajout footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Footer.css'
+import NewsLetter from "./Newsletter";
+import Socials from "./Socials";
+import {Col, Container, Row} from "react-bootstrap";
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container className="justify-content-around">
+                <Row className="rowFooter">
+                    <Col>
+                        <NewsLetter/>
+                    </Col>
+                    <Col>
+                        <Socials className="socials"/>
+                    </Col>
+                </Row>
+            </Container>
+            <div className="copyright">2021 Sous mon toit</div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from "../../components/Header/Header";
 import Menu from "../../navigation/Menu/Menu";
-
-const HomeWrapper = styled.div`
-    // display: flex;
-    // justify-content: center;
-`
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
     return (
-        <HomeWrapper>
+        <div>
             <Header/>
             <Menu/>
-        </HomeWrapper>
+            <Footer/>
+        </div>
     );
 };
 
