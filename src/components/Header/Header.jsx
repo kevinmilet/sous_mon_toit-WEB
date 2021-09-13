6ce9bc935f4e8c678c68ed7853b07294a6b2a4ba
Kevin Milet <kevin.milet@gmail.com>
2021-09-13 14:52:44 +0200

    kevin - ajout header avec logo + menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,54 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
-// import {Link} from "react-router-dom";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
-
-const HomeLogo = styled.img`
-    width: 390px;
-    height: 100px;
-    margin-left: 150px;
-`
-//
-// const ConectLink = styled.a`
-//     font-size: 24px;
-//     text-decoration: none;
-//     text-align: center;
-//     color: #707070;
-//     margin-right: 150px;
-//     &:hover {
-//     color: #E85A70;
-//   }
-// `
-//
-// const NavContainer = styled.nav`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-// `
+import {Col, Container, Row} from "react-bootstrap";
+import logo from '../../assets/img/logo_sousMonToit_Long.png'
+import './Header.css';
 
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-
-
+        <Container fluid>
+            <Row>
+                <Col className="logoCol">
+                    <img src={logo} className="logo" alt="Logo Sous Mon Toit"/>
+                </Col>
+                <Col className="linkCol">
+                    {/*<Link href="#" className="connectLink">Se connecter</Link>*/}
+                    <a href="#" className="connectLink">Se connecter</a>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from "../../components/Header/Header";
+import Menu from "../../navigation/Menu/Menu";
 
 const HomeWrapper = styled.div`
     // display: flex;
@@ -11,6 +12,7 @@ const Home = () => {
     return (
         <HomeWrapper>
             <Header/>
+            <Menu/>
         </HomeWrapper>
     );
 };
