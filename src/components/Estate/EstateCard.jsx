4f85cb8724482a6afcaaa7f69152d3d26ebecea8
Ethan <ethan-cda@MacBook-Pro-de-Eldib.local>
2021-09-14 10:18:32 +0200

    Ethan - modification du fichier EstateCard.jsx

--- src/components/Estate/EstateCard.jsx
+++ src/components/Estate/EstateCard.jsx
@@ -2,31 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import {Link} from "react-router-dom";
 
+const CardImage = styled.img`
+  height: auto;
+  width: 100%;
+  align-self: center;
+`
+
 const EstateCard = () => {
     return (
         <div>
-            <ul>
-                <li className={"nav-link"}>
-                    <Link to={'/'}>Accueil</Link>
-                </li>
-            </ul>
-
-            <div className={"container"}>
-                <div className={"card w-25"}>
-                    <div className={"card-top"}>
-                        <img src="/images/heart.png" alt="favoris"/>
+            <div className="card text-center w-25">
+                <div className="card-header">
+                    <div className={"d-flex justify-content-around"}>
+                        299 000 €
+                        <i className="far fa-heart"/>
                     </div>
-
-                    <div className={"bottom"}>
-                        <h3 className={"m-2"}>299 000 €</h3>
-                        <img src="https://i.ibb.co/rf2TbH8/home-office-5006842-1280.png"
-                             className={"img-thumbnail"}
-                             alt="bien immobilier n° 1469"/>
-                        <h2 className={"title h6 text-uppercase m-2"}>60240 Chaumont</h2>
-                        <p className={"text text-uppercase m-2"}>
-                            À vendre maison 10 pièces 230 m<sup>2</sup>
-                        </p>
-                    </div>
+                </div>
+                <CardImage src={"https://i.ibb.co/4WN1sny/pexels-binyamin-mellish-106399.jpg"} alt="bien immobilier n° 1469" />
+                <div className="card-body">
+                    {/* TODO slider */}
+                </div>
+                <div className="card-footer text-muted">
+                    <p className={"text-uppercase text-dark m-2"}>60240 Chaumont</p>
+                    <p className={"text-uppercase text-dark m-2"}>
+                        À vendre maison 10 pièces 230 m<sup>2</sup>
+                    </p>
                 </div>
             </div>
         </div>
