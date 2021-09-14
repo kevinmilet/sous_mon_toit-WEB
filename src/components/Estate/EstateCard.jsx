63212cab83aba537943e8807c92b5693e7321ed1
Ethan <ethan-cda@MacBook-Pro-de-Eldib.local>
2021-09-14 09:34:39 +0200

    Ethan - ajout des cartes pour les biens immobilier + route /liste-des-biens

---
+++ src/components/Estate/EstateCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from "react-router-dom";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+const EstateCard = () => {
+    return (
+        <div>
+            <ul>
+                <li className={"nav-link"}>
+                    <Link to={'/'}>Accueil</Link>
+                </li>
+            </ul>
+
+            <Container>
+                <div className={"card w-25"}>
+                    <div className={"card-top"}>
+                        <img src="/images/heart.png" alt="favoris"/>
+                    </div>
+
+                    <div className={"bottom"}>
+                        <h3 className={"m-2"}>299 000 €</h3>
+                        <img src="https://i.ibb.co/rf2TbH8/home-office-5006842-1280.png"
+                             className={"img-thumbnail"}
+                             alt="bien immobilier n° 1469"/>
+                        <h2 className={"title h6 text-uppercase m-2"}>60240 Chaumont</h2>
+                        <p className={"text text-uppercase m-2"}>
+                            À vendre maison 10 pièces 230 m<sup>2</sup>
+                        </p>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default EstateCard;
