179d43c68185a364ac3b6f9e76ff0c1bd9a5b445
laurentvac <laurentvac80@gmail.com>
2021-09-14 10:07:48 +0200

    maj bootstrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
 import logo from '../../assets/img/logo_sousMonToit_Long.png'
 import './Header.css';
 
 const Header = () => {
     return (
-        <Container fluid>
-            <Row className="rowHeader">
-                <Col className="logoCol">
+        <div className="container-fluid">
+            <div className="row rowHeader">
+                <div className="col logoCol">
                     <img src={logo} className="logo" alt="Logo Sous Mon Toit"/>
-                </Col>
-                <Col className="linkCol">
+                </div>
+                <div className="col linkCol">
                     {/*<Link href="#" className="connectLink">Se connecter</Link>*/}
                     <a href="#" className="connectLink">Se connecter</a>
-                </Col>
-            </Row>
-        </Container>
+                </div>
+            </div>
+        </div>
     );
 };
 
