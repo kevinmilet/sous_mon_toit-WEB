21af51b1fa74ef381c9faabc14e559b59da8fae3
Ethan <ethan-cda@MacBook-Pro-de-Eldib.local>
2021-09-14 15:12:27 +0200

    Ethan - ajout du Header + Footer pour l'url /liste-des-biens

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Home from "./screens/Home/Home";
 import EstateCard from "./components/Estate/EstateCard";
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
@@ -11,7 +13,9 @@ const App = () => {
                     <Home/>
                 </Route>
                 <Route exact path="/liste-des-biens">
+                    <Header/>
                     <EstateCard/>
+                    <Footer/>
                 </Route>
             </Router>
         </div>
