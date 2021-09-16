c06b026577ddd830c57ee1492e6288cc42980867
Kevin Milet <kevin.milet@gmail.com>
2021-09-16 09:15:11 +0200

    kevin - correction sur App.jsx

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Footer from "../../components/Footer/Footer";
 import MainHome from "../../components/MainHome/MainHome";
-import Header from "../../components/Header/Header";
 
 const Home = () => {
     return (
         <div>
-            <Header/>
             <MainHome/>
-            <Footer/>
         </div>
     );
 };
