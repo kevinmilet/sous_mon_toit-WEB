7ff160ef8d04b0c755ca51e5cdcdb9e28e54732d
Kevin Milet <kevin.milet@gmail.com>
2021-09-17 11:40:00 +0200

    kevin - ajout style global pour élements html

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -8,16 +8,15 @@ import {StyledBtnPrimary, StyledInput} from "../../utils/styles/Atoms";
 
 const ConnexionForm = styled.form`
     background-color: ${colors.backgroundPrimary};
+    -webkit-box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+    -moz-box-shadow:    0px 3px 6px rgba(0, 0, 0, 0.16);
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
 `
 const ConnexiontH1 = styled.h1`
     color: ${colors.secondary};
 `
 const ConnexionLabel = styled.label`
     color: ${colors.secondaryBtn};
-`
-const ConnexionBtn = styled.button`
-    background-color: ${colors.primaryBtn};
-    color: #fff;
 `
 
 const SignIn = () => {
